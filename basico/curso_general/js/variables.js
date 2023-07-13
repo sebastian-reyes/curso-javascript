336857a089b641c1c184f661331a6c21e4bf4d45
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2023-07-12 20:18:01 -0500

    Variables - modificando nombre de archivo.js

---
+++ basico/curso_general/js/variables.js
@@ -0,0 +1,39 @@
+console.log("Esto viene desde code.js");
+
+//1 - VARIABLES Y CONSTANTES
+
+//Varibales: su valor puede variar
+let nombre ="sebastián"
+nombre = "josé"
+console.log(nombre);
+let casa, casa2, casa3;
+
+casa = "perez"
+casa2 = "castell"
+casa3 = "ruiz"
+
+//Constantes: no se puede cambiar su valor
+const DIAS_SEMANA = 7
+//DIAS_SEMANA = 10 (Error)
+
+//2 - DATOS PRIMITIVOS
+let num = 10
+let cadena = "esto es 'una' cadena"
+let boolean = true
+console.log(cadena);
+console.log(typeof num);
+console.log(typeof cadena);
+console.log(typeof boolean);
+
+let indefinido;
+console.log(indefinido);
+console.log(typeof indefinido);
+
+//3 - COMENTARIOS
+
+//Esto es un comentario en una línea
+
+/* Esto es un comentario en bloque
+console.log(indefinido);
+console.log(typeof num);
+*/
