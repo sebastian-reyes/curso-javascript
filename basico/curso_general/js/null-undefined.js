e82286ea09299022174b8326e457db75ecb70dd0
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2023-07-14 09:40:32 -0500

    Básico - null vs undefined

---
+++ basico/curso_general/js/null-undefined.js
@@ -0,0 +1,15 @@
+/*
+
+NULL - Que no contiene ningún valor, pero fue asignado con alguna INTENCIÓN
+UNDEFINED - Que no tiene ningún valor asignado, nadie le dio un valor
+
+*/
+
+let usuarioLogeado = null; //El valor no es nada, ni true, ni false, ni -1
+console.log(usuarioLogeado);
+
+let indefinido;
+console.log(indefinido);
+
+let sinDefinir = undefined;
+console.log(sinDefinir);
