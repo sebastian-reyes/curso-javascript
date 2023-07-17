197b186617d6e31bc096ba3f4e68f8ab6c6af80c
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2023-07-17 08:25:32 -0500

    Básico - Objeto Math

---
+++ basico/curso_general/js/ObjetoMath.js
@@ -0,0 +1,30 @@
+console.log("Objeto Math");
+
+//Obtener PI
+console.log(Math.PI);
+
+//Mostrar solo 2 decimales
+console.log(Math.PI.toFixed(2));
+
+//Quitar los decimales
+console.log(Math.floor(Math.PI));
+
+//Redondear un número
+console.log(Math.round(4.6));
+console.log(Math.round(Math.PI));
+
+//Pasar un número negativo a su valor absoluto positivo
+console.log(Math.abs(-451));
+
+//Calcular la potencia (base, exponente)
+console.log(Math.pow(2, 3));
+console.log(2 ** 3);
+
+//Crear un número aleatorio entre 0 y 1 (no incluye el 1)
+console.log(Math.random());
+
+//Crear un número positivo entre 0 y 10.algo
+console.log(Math.random() * 10 + 1);
+
+//Crear un número positivo entre 0 y 10, sin deicmales
+console.log(Math.floor(Math.random() * 10 + 1));
