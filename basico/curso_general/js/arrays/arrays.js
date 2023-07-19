0043937f244c02f27a08707cfe4bd551705177b9
sebastian.reyes <jose.reyes.quiroz.contractor@bbva.com>
2023-07-18 22:34:35 -0500

    Básico- arrays

---
+++ basico/curso_general/js/arrays/arrays.js
@@ -0,0 +1,11 @@
+let cadena = "Hola";
+let cadena2 = new String("Hola mundo");
+
+let miarray = ["Hola", 2, 3, null, true, 0.25];
+let miSegundoArray = new Array(1, 2, true, false);
+
+console.log(miarray);
+console.log(miSegundoArray);
+
+console.log(miarray[3]);
+console.log(miSegundoArray[5]);
